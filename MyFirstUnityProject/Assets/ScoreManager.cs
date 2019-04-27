bdeeb08a3a1c1de05dc801e187d2c0b8b4535211
berry <j890093@gmail.com>
2019-04-27 18:10:00 +0800

    20190427_part1

---
+++ MyFirstUnityProject/Assets/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	private Text m_kTextBox;
+	private int m_fCurentScore;
+	const string DF_SCORE_PREFIX = "Score :";
+	// Use this for initialization
+	void Start () {
+		m_kTextBox = this.GetComponent<Text> ();
+		SetScore ();
+	}
+
+	public void AddScore (int _nScore)
+	{
+		m_fCurentScore += _nScore;
+		SetScore ();
+	}
+
+	void SetScore ()
+	{
+		m_kTextBox.text = DF_SCORE_PREFIX + m_fCurentScore;
+	}
+}
