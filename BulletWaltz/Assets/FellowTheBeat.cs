4ac413b770f7bbcb17f6f2b670f2a25e7b2c8325
berry <j890093@gmail.com>
2019-05-04 17:15:25 +0800

    New Project BulletWaltz to PPT Week03 Page.33

---
+++ BulletWaltz/Assets/FellowTheBeat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FellowTheBeat : MonoBehaviour {
+
+	private const float DF_BEAT_PERIOD = 1.485f;
+	private const float DF_SHOOT_PASS_OFFEST = DF_BEAT_PERIOD * 3;
+	private const float DF_ROTATE_COUNTER_OFFESET = 0.2f;
+	private const float DF_SHOOT_COUNTER_OFFESET = -0.5f;
+	private float m_fRotateCounter;
+	private float m_fShootCounter;
+	private TurrentManagerScript m_kTurrent;
+	// Use this for initialization
+	void Start () {
+		m_fRotateCounter = DF_ROTATE_COUNTER_OFFESET;
+		m_fShootCounter = DF_SHOOT_COUNTER_OFFESET + m_fShootCounter - DF_SHOOT_PASS_OFFEST;
+		m_kTurrent = this.GetComponent <TurrentManagerScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_fRotateCounter += Time.deltaTime;
+		m_fShootCounter += Time.deltaTime;
+
+		if (m_fRotateCounter > DF_BEAT_PERIOD) {
+			m_kTurrent.PlayRotateAnimation ();
+			m_fRotateCounter -= DF_BEAT_PERIOD;
+		}
+		if (m_fShootCounter > DF_BEAT_PERIOD) {
+			m_kTurrent.PlayShootAnimation ();
+			m_fShootCounter -= DF_BEAT_PERIOD;
+		}
+	}
+}
---
+++ BulletWaltz/Assets/TurrentManagerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TurrentManagerScript : MonoBehaviour {
+
+	private Animator m_kAnimator;
+	int DF_DIRECTION_COUNT = 8;
+	public Ease EaseFunction;
+	public float RotateDuration;
+	public Camera MainCamera;
+	public float CameraShakeDuration;
+	public float CameraShakeStrength;
+	// Use this for initialization
+	void Start () {
+		m_kAnimator = this.GetComponent <Animator> ();
+	}
+	
+	public void PlayShootAnimation ()
+	{
+		m_kAnimator.SetTrigger ("Shoot");
+		MainCamera.transform.DOShakePosition (CameraShakeDuration, CameraShakeStrength);
+	}
+
+	public void PlayRotateAnimation ()
+	{
+		float targetDegree = 360.0f / DF_DIRECTION_COUNT * Random.Range (0, DF_DIRECTION_COUNT);
+		this.transform.DORotate (new Vector3 (0, 0, targetDegree), RotateDuration);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			PlayShootAnimation ();
+		}
+		if (Input.GetKeyDown (KeyCode.R)) {
+			PlayRotateAnimation ();
+		}
+	}
+}
