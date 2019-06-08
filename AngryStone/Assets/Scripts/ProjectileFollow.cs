f35da6f42053de7779e8c5b4c533af8362215873
berry <j890093@gmail.com>
2019-06-08 18:46:03 +0800

    Finish AngerStone Part2, unless particleSystem

---
+++ AngryStone/Assets/Scripts/ProjectileFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileFollow : MonoBehaviour {
+
+	public Transform projectile;
+	public Transform LeftBound;
+	public Transform RightBound;
+	void Update () {
+		Vector3 newPosition = transform.position;
+		newPosition.x = projectile.position.x;
+		newPosition.x = Mathf.Clamp (newPosition.x, LeftBound.position.x, RightBound.position.x);
+		transform.position = newPosition;
+	}
+}
---
+++ AngryStone/Assets/Scripts/Reseter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reseter : MonoBehaviour {
+
+	public Rigidbody2D projecTile;
+	private float resetSpeed = 0.25f;
+	private float resetSpeedSqr;
+	private SpringJoint2D Spring; 
+	void Start () {
+		resetSpeedSqr = resetSpeed * resetSpeed;
+		Spring = projecTile.GetComponent <SpringJoint2D> ();
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Reset ();
+		}
+
+		if (Spring == null && projecTile.velocity.magnitude < resetSpeedSqr) {
+			Reset ();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.GetComponent <Rigidbody2D> () == projecTile) {
+			Reset ();
+		}	
+	} 
+	void Reset () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+}
